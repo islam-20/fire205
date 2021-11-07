7d96cdd83c29797713879e32106086b2bf623142
islam-20055 <88165118+islam-20055@users.noreply.github.com>
2021-11-07 22:34:13 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -4322,7 +4322,7 @@ keyboard.inline_keyboard = {
 {{text ='⌯الــبــوتــات•', callback_data="/gqjik"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/UU_f7&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/QueenfOO&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 
 if text == 'اسلام' or text == 'المبرمج اسلام ' or text == 'مبرمج السورس' or text == 'يا نقيب' then
